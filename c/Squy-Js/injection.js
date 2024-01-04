6df308c40a36ee7a9548315970197deaa00f224e
unxbiq <154544380+unxbiq@users.noreply.github.com>
2024-01-04 19:05:44 +0700

    Update injection.js

--- c/Squy-Js/injection.js
+++ c/Squy-Js/injection.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
         // Mengambil data dari file JSON
         $.getJSON('../images.json', function(data){
-            $('.tekss24').text(data[0].teks1);
+            $('.tekss24').text(data[0].teks1)
         });
     });
