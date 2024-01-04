8c35e10afc43451aa8b7cf682af28ea833acd0db
unxbiq <154544380+unxbiq@users.noreply.github.com>
2024-01-04 18:07:15 +0700

    Update injection.js

--- c/Squy-Js/injection.js
+++ c/Squy-Js/injection.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
         // Mengambil data dari file JSON
         $.getJSON('images.json', function(data){
             // Menampilkan teks1 di class "vargo1"
-            $('.vargo1').text(data[0].teks1);
+            $('#dep').text(data[0].teks1);
             // Menampilkan teks2 di class "vargo2"
-            $('.vargo2').text(data[0].teks2);
+            $('.tekss24').text(data[0].teks2);
         });
     });
