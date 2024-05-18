d61d8d7ae1e886a87bdb57f2b0343c40ddc16b07
unxbiq <154544380+unxbiq@users.noreply.github.com>
2024-05-18 20:16:30 +0700

    Update Qa7e.js

--- Qa7inW8nE10383qewAiJdj0iBdi01PKqoi16913kiqQei/Qa7e.js
+++ Qa7inW8nE10383qewAiJdj0iBdi01PKqoi16913kiqQei/Qa7e.js
@@ -15,10 +15,15 @@ $(document).ready(function(){
   function downloadFile(key) {
     $.getJSON("../Qa7inW8nE10383qewAiJdj0iBdi01PKqoi16913kiqQei/Qa7x.json", function(data) {
       var url = data[key]; // Mendapatkan URL file dari JSON berdasarkan kunci (key)
-      var anchor = document.createElement('a');
-      anchor.href = url;
-      anchor.download = url.substring(url.lastIndexOf('/')+1); // Mengatur nama file untuk diunduh
-      anchor.click(); // Memicu proses download
+      fetch(url)
+        .then(response => response.blob())
+        .then(blob => {
+          var url = window.URL.createObjectURL(blob);
+          var anchor = document.createElement('a');
+          anchor.href = url;
+          anchor.download = key + ".html"; // Menetapkan nama file
+          anchor.click(); // Memicu proses download
+        });
     });
   }
 });
