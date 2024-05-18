33d00c4c2eca2ff386cf21dfd27039af27017ab7
unxbiq <154544380+unxbiq@users.noreply.github.com>
2024-05-18 20:37:00 +0700

    Update Qa7e.js

--- Qa7inW8nE10383qewAiJdj0iBdi01PKqoi16913kiqQei/Qa7e.js
+++ Qa7inW8nE10383qewAiJdj0iBdi01PKqoi16913kiqQei/Qa7e.js
@@ -1,18 +1,18 @@
 // TOMBOL UNDUH
 $(document).ready(function(){
   $("#btn01").click(function(){
-    downloadFile("sc1");
+    downloadFile("sc1", "zip");
   });
 
   $("#btn02").click(function(){
-    downloadFile("sc2");
+    downloadFile("sc2", "zip");
   });
   
   $("#btn03").click(function(){
-    downloadFile("sc3");
+    downloadFile("sc3", "zip");
   });
 
-  function downloadFile(key) {
+  function downloadFile(key, fileType) {
     $.getJSON("../Qa7inW8nE10383qewAiJdj0iBdi01PKqoi16913kiqQei/Qa7x.json", function(data) {
       var url = data[key]; // Mendapatkan URL file dari JSON berdasarkan kunci (key)
       fetch(url)
@@ -21,7 +21,7 @@ $(document).ready(function(){
           var url = window.URL.createObjectURL(blob);
           var anchor = document.createElement('a');
           anchor.href = url;
-          anchor.download = key + ".html"; // Menetapkan nama file
+          anchor.download = key + "." + fileType; // Menetapkan nama file dengan ekstensi yang sesuai
           anchor.click(); // Memicu proses download
         });
     });
